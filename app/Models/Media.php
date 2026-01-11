417da7dfe1d6e4a592eff1ff04642a5e8555e96b
Primož Novak <primoz.novak@impol.si>
2026-01-11 21:05:14 +0100

    Add 'originalPath' and 'thumbnailPath' attribute casting

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -1,8 +1,10 @@
 <?php namespace App\Models;
 
 use App\Enum\MediaStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class Media extends Model
 {
@@ -30,5 +32,19 @@ class Media extends Model
     public function uploader(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function originalPath(): Attribute
+    {
+        return Attribute::make(
+            get: fn (?string $value) => $value ? Storage::disk('public')->url($value) : null,
+        );
+    }
+
+    public function thumbnailPath(): Attribute
+    {
+        return Attribute::make(
+            get: fn (?string $value) => $value ? Storage::disk('public')->url($value) : null,
+        );
     }
 }
