4a2b6dfc7fc0dcc84593f5a9e0106b546baa73c2
Primož Novak <primoz.novak@impol.si>
2026-01-11 17:14:18 +0100

    Add 'get-token' route and 'login' placeholder in 'web.php'

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    abort(501, 'Not implemented yet');
+})->name('login');
+
+Route::get('/get-token', function () {
+
+    abort_if(!app()->isLocal(), 404);
+
+    $user = \App\Models\User::firstOrCreate(
+        ['email' => 'api@example.com'],
+        [
+            'name' => 'API User',
+            'password' => bcrypt('password'),
+        ]
+    );
+
+    // Optional: revoke old tokens so you always get a fresh one
+    $user->tokens()->delete();
+
+    return $user->createToken('media-api')->plainTextToken;
+});;
